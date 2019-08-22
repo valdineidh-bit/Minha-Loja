0726a76f402a8f411788cc057d5f57ed7d81809f
valdineidh-bit <valdineidh40@gmail.com>
2019-08-22 16:33:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-x4sefe-2tmuma
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-x4sefe-2tmuma)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Telefone XL',
+    price: 799,
+    description: 'Um telefone grande com uma das melhores telas'
+  },
+  {
+    name: 'Telefone Mini',
+    price: 699,
+    description: 'Um ótimo celular com uma das melhores câmeras'
+  },
+  {
+    name: 'Padrão do telefone',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
